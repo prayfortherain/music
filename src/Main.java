07342958e0bf5ada613e577cb4076c16be1ef17b
prayfortherain <prayfortherain@yandex.ru>
2023-11-04 17:06:18 +0300

    add JLayer, save pictures, play song

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,48 @@
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.Player;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        String a = "https://rur.hitmotop.com/get/music/20230110/Wichu_kujopjop_Wichu_kujopjop_kujopjop_Wichu_-_ZXC_DOTA2_GHOUL_SSS_RANK_75371926.mp3";
-        URL website = new URL(a);
-        ReadableByteChannel rbc = Channels.newChannel(website.openStream());
-        FileOutputStream fos = new FileOutputStream("dota2.mp3");
-        fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+    public static void main(String[] args) {
+        Thread saveMp3 = new Thread(() -> {
+            try {
+                URL website = new URL("https://rur.hitmotop.com/get/music/20201219/HEALTH_Window_Weather_-_Major_Crimes_72017415.mp3");
+                ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+                FileOutputStream fos = new FileOutputStream("Major_crimes.mp3");
+                fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Скачивание музыки завершено. \nВоспроизводим.");
+            playMp3("C:\\Users\\acer\\IdeaProjects\\untitled6\\Major_crimes.mp3");
+        });
+        Thread savePic = new Thread(() -> {
+            try {
+                URL website = new URL("https://i.pinimg.com/564x/c0/f8/fd/c0f8fd69e0f80d1150dc7725a5200b9c.jpg");
+                ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+                FileOutputStream fos = new FileOutputStream("pic.jpg");
+                fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Скачивание картинки завершено.");
+        });
+        saveMp3.start();
+        savePic.start();
+    }
+
+    public static void playMp3(String filePath) {
+        try (FileInputStream inputStream = new FileInputStream(filePath)) {
+            Player player = new Player(inputStream);
+            player.play();
+            System.out.println("Песенка кончилась.");
+        } catch (IOException | JavaLayerException e) {
+            e.printStackTrace();
+        }
     }
 }
